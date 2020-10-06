6e2edd85305aa5ded33aac8ced54fbaade185102
Jam Wheat <jamonwheat@gmail.com>
2020-10-06 17:09:51 -0400

    fix static serving issue

--- boardgamecollector/settings.py
+++ boardgamecollector/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import environ
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    # DEBUG=(bool, False)
 )
 # reading .env file
 environ.Env.read_env()
@@ -30,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
