bfacbf9b87fe11dadc6c414e390de8f35665c8fe
Jam Wheat <jamonwheat@gmail.com>
2020-10-06 16:05:39 -0400

    create Game model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+  name = models.CharField(max_length=100)
+  publisher = models.CharField(max_length=100)
+  designer = models.CharField(max_length=100)
+  pub_year = models.IntegerField()
+
+  def __str__(self):
+    return self.name
