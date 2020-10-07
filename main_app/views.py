3e4d06529f6d422a4a7f56e596aab288c0ab2c1d
Jam Wheat <jamonwheat@gmail.com>
2020-10-07 14:37:30 -0400

    scaffold update/delete game buttons

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Game
-from django.views.generic.edit import CreateView
 
 # Create your views here.
 
@@ -21,3 +21,11 @@ def games_detail(request, game_id):
 class GameCreate(CreateView):
   model = Game
   fields = '__all__'
+
+class GameUpdate(UpdateView):
+  model = Game
+  fields = '__all__'
+
+class GameDelete(DeleteView):
+  model = Game
+  success_url = '/games/'
